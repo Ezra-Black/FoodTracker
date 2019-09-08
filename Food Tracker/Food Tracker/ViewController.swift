ad256a20fdf7862a4f066cbfa47c4b333b68024b
JosephRogers94 <Joerogersss2016@gmail.com>
2019-09-08 17:34:39 -0500

    Base Project

---
+++ Food Tracker/Food Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Joseph Rogers on 9/8/19.
+//  Copyright © 2019 Joe Rogers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
